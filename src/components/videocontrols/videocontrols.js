db107a8c161faea17b87a339e660feab4267390a
timon <timon0596@yandex.ru>
2019-12-14 22:31:09 +0300

    videoplayer

--- src/components/videocontrols/videocontrols.js
+++ src/components/videocontrols/videocontrols.js
@@ -1,5 +1,11 @@
 let video = $('video')[0]
-console.log(video)
+let currenttime = $('.controls .currenttime_range')[0]
+
+video.ontimeupdate = function(){
+	currenttime.style.width = this.currentTime/this.duration*100 + '%'
+	video.ended?$('.playpause_btn').toggleClass('paused'):0
+
+}
 $('.videoplayer .play_btn').click(function(){
 	$('.videoplayer .preview').css('display', 'none')
 	$(this).css('display','none')
@@ -13,4 +19,12 @@ $('.playpause_btn').click(function(){
 		video.pause()
 	}
 	$(this).toggleClass('paused')
+})
+$('.videoplayer .videoduration_range').click(function(e){
+	let clickedTimestamp = (e.clientX - $(this)[0].getBoundingClientRect().left)/parseFloat($(this).parent().css('width'))
+	console.log(parseFloat($(this).parent().css('width')))
+	console.log($(this)[0].getBoundingClientRect().left)
+	video.pause()
+	video.currentTime = video.duration * clickedTimestamp
+	video.play()
 })
