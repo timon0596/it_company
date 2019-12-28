3273b162c0e5e866d73d22b8b98577c2abe04811
tim <timon0596@yandex.ru>
2019-12-28 06:19:32 +0300

    footer

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,8 @@ module.exports={
 					            	require('precss'),
 					            	require('autoprefixer')
 				          		];
-				        	}
+				        	},
+				        	useRelativePath: true
 				      	}
 				    },
 				    {
