8ec1747ff70122b28b178a0e276e0dc7b9aa528b
timon <timon0596@yandex.ru>
2019-12-30 06:14:34 +0300

    header responsive

---
+++ smartgrid.js
@@ -0,0 +1,56 @@
+const smartGrid = require('smart-grid');
+const path = require('path');
+const options = {
+    filename: "smart-grid",
+    outputStyle: "sass",
+    columns: 12,
+    offset: "30px",
+    mobileFirst: false,
+    container: {
+        maxWidth: "1280px",
+        fields: "30px"
+    },
+    breakPoints: {
+        lg: {
+            width: "1200px"
+        },
+        md: {
+            width: "992px",
+            fields: "15px"
+        },
+        sm: {
+            width: "720px"
+        },
+        xs: {
+            width: "576px"
+        }
+    },
+    mixinNames: {
+        container: "wrapper",
+        row: "row-flex",
+        rowFloat: "row-float",
+        rowInlineBlock: "row-ib",
+        rowOffsets: "row-offsets",
+        column: "col",
+        size: "size",
+        columnFloat: "col-float",
+        columnInlineBlock: "col-ib",
+        columnPadding: "col-padding",
+        columnOffsets: "col-offsets",
+        shift: "shift",
+        from: "from",
+        to: "to",
+        fromTo: "from-to",
+        reset: "reset",
+        clearfix: "clearfix",
+        debug: "debug",
+        uRowFlex: "u-row-flex",
+        uColumn: "u-col",
+        uSize: "u-size"
+    },
+    tab: "    ",
+    defaultMediaDevice: "screen",
+    detailedCalc: false
+};
+ 
+smartGrid(path.resolve(__dirname,'src'), options);
